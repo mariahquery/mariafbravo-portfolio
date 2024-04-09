7cf456cf046272d263b56764bb014cb04578894d
Maria Furtado Bravo <maria.bravo@koerber.com>
2024-04-09 23:31:24 +0100

    added favicon

--- app/src/components/sections/PortfolioSection.vue
+++ app/src/components/sections/PortfolioSection.vue
@@ -1,175 +1,176 @@
-<script setup></script>
-
 <template>
-  <!-- <section id="portfolio">
+  <section id="portfolio">
     <div class="container">
-      <h1 class="title is-3">Portfolio</h1>
-      <p class="subtitle">
-        Here are some of the projects I've worked on. I've designed and
-        developed websites, web apps, and mobile apps for a variety of clients
-        and industries like Retail, Logistics, Recruitment and Healthcare.
-      </p>
+      <div class="column">
+        <h1 class="title is-3">Portfolio</h1>
+        <p class="subtitle">
+          Here are some of the projects I've worked on. I've designed and
+          developed websites, web apps, and mobile apps for a variety of clients
+          and industries like Retail, Logistics, Recruitment and Healthcare.
+        </p>
+      </div>
       <div class="columns">
-      <div class="column">
-        <div class="grid">
-          <figure class="effect-sadie">
-            <img
-              src="../../assets/portfolio/portfolio-img-1.jpg"
-              alt="EUREKATHON"
-            />
-            <figcaption>
-              <h2>EUREKATHON</h2>
-              <p>
-                Eurekathon is a data-driven competition that addresses societal
-                issues associated with sustainable development goals supported
-                by the European Union. I designed and developed Eurekathon's all
-                three editions:
-                <a href="https://www.eurekathon.com/" target="_blank">2021</a>,
-                <a
-                  href="https://www.eurekathon.com/2020/index.html"
-                  target="_blank"
-                  >2020</a
-                >
-                and
-                <a
-                  href="https://www.eurekathon.com/2019/index.html"
-                  target="_blank"
-                  >2019</a
-                >.
-              </p>
-            </figcaption>
-          </figure>
-        </div>
-      </div>
-      <div class="column">
-        <div class="grid">
-          <a href="https://talentportugal.com/" target="_blank">
+        <div class="column">
+          <div class="grid">
             <figure class="effect-sadie">
               <img
-                src="../../assets/portfolio/portfolio-img-2.jpg"
-                alt="Talent Portugal"
+                src="../../assets/portfolio/portfolio-img-1.jpg"
+                alt="EUREKATHON"
               />
               <figcaption>
-                <h2>Talent Portugal</h2>
+                <h2>EUREKATHON</h2>
                 <p>
-                  Talent Portugal is a recruitment platform that connects
-                  companies with the best talent in Portugal. I developed the
-                  User Interface of the platform when I worked at Webcomum,
-                  which includes a job board, company profiles, and a candidate
-                  database.
+                  Eurekathon is a data-driven competition that addresses
+                  societal issues associated with sustainable development goals
+                  supported by the European Union. I designed and developed
+                  Eurekathon's all three editions:
+                  <a href="https://www.eurekathon.com/" target="_blank">2021</a
+                  >,
+                  <a
+                    href="https://www.eurekathon.com/2020/index.html"
+                    target="_blank"
+                    >2020</a
+                  >
+                  and
+                  <a
+                    href="https://www.eurekathon.com/2019/index.html"
+                    target="_blank"
+                    >2019</a
+                  >.
                 </p>
               </figcaption>
             </figure>
-          </a>
+          </div>
+        </div>
+        <div class="column">
+          <div class="grid">
+            <a href="https://talentportugal.com/" target="_blank">
+              <figure class="effect-sadie">
+                <img
+                  src="../../assets/portfolio/portfolio-img-2.jpg"
+                  alt="Talent Portugal"
+                />
+                <figcaption>
+                  <h2>Talent Portugal</h2>
+                  <p>
+                    Talent Portugal is a recruitment platform that connects
+                    companies with the best talent in Portugal. I developed the
+                    User Interface of the platform when I worked at Webcomum,
+                    which includes a job board, company profiles, and a
+                    candidate database.
+                  </p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <div class="grid">
+            <a
+              href="https://ltplabs.com/featured-insights/a-marketplace-for-analytics-solutions/"
+              target="_blank"
+            >
+              <figure class="effect-sadie">
+                <img
+                  src="../../assets/portfolio/portfolio-img-3.jpg"
+                  alt="Talent Portugal"
+                />
+                <figcaption>
+                  <h2>AIR - Advanced Analytics Platform</h2>
+                  <p>
+                    AIR is a platform that offer analytics solutions based on AI
+                    models for companies to improve their business. I designed
+                    it with Adobe XD and developed is using
+                    <strong>HTML5</strong>, <strong>CSS3 (SCSS)</strong>,
+                    <strong>JavaScript</strong>, <strong>Vue.js</strong>,
+                    <strong>Git</strong>, <strong>Chart.js</strong>,
+                    <strong>GraphQL</strong>, and
+                    <strong>RESTful Services</strong> when working at LTPlabs.
+                  </p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+        <div class="column">
+          <div class="grid">
+            <a
+              href="https://www.behance.net/gallery/179360569/Koerber-Design-System-missing-protected-data"
+              target="_blank"
+            >
+              <figure class="effect-sadie">
+                <img
+                  src="../../assets/portfolio/portfolio-img-4.jpg"
+                  alt="Körber Design System"
+                />
+                <figcaption>
+                  <h2>Körber Design System</h2>
+                  <p>
+                    Körber Design System is a Design System for the all Körber
+                    digital products. I designed and developed some of the
+                    Design System guidelines and components using
+                    <strong>>Web components</strong> with
+                    <strong>Lit</strong>,<strong>Typescript</strong> and
+                    <strong>Figma</strong>.
+                  </p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column">
+          <div class="grid">
+            <a href="https://www.susanasoarespinto.eu/" target="_blank">
+              <figure class="effect-sadie">
+                <img
+                  src="../../assets/portfolio/portfolio-img-5.jpg"
+                  alt="Susana Soares Pinto"
+                />
+                <figcaption>
+                  <h2>Susana Soares Pinto</h2>
+                  <p>
+                    Susana Soares Pinto is a portuguese multimedia and plastic
+                    artist. I designed and developed her website to showcase her
+                    work and exhibitions. The website includes a gallery of her
+                    work with a <strong>CMS</strong> to manage her own content
+                    autonomously.
+                  </p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
+        </div>
+        <div class="column">
+          <div class="grid">
+            <a href="https://trustai.eu/" target="_blank">
+              <figure class="effect-sadie">
+                <img
+                  src="../../assets/portfolio/portfolio-img-6.jpg"
+                  alt="Trust AI"
+                />
+                <figcaption>
+                  <h2>Trust AI</h2>
+                  <p>
+                    Trust AI is a European Union project that I developing while
+                    working for LTPlabs. It presents a transparent, reliable and
+                    unbiased smart AI tool that's applied in specific use cases.
+                    I designed and developed the website with
+                    <strong>HTML5</strong>, <strong>SCSS</strong>,
+                    <strong>JavaScript</strong> and
+                    <strong>Jekyll</strong> while working at LTPlabs.
+                  </p>
+                </figcaption>
+              </figure>
+            </a>
+          </div>
         </div>
       </div>
     </div>
-    <div class="columns">
-      <div class="column">
-        <div class="grid">
-          <a
-            href="https://ltplabs.com/featured-insights/a-marketplace-for-analytics-solutions/"
-            target="_blank"
-          >
-            <figure class="effect-sadie">
-              <img
-                src="../../assets/portfolio/portfolio-img-3.jpg"
-                alt="Talent Portugal"
-              />
-              <figcaption>
-                <h2>AIR - Advanced Analytics Platform</h2>
-                <p>
-                  AIR is a platform that offer analytics solutions based on AI models for
-                  companies to improve their business. I
-                  designed it with Adobe XD and developed is using
-                  <strong>HTML5</strong>, <strong>CSS3 (SCSS)</strong>,
-                  <strong>JavaScript</strong>, <strong>Vue.js</strong>,
-                  <strong>Git</strong>, <strong>Chart.js</strong>,
-                  <strong>GraphQL</strong>, and
-                  <strong>RESTful Services</strong> when working at LTPlabs.
-                </p>
-              </figcaption>
-            </figure>
-          </a>
-        </div>
-      </div>
-      <div class="column">
-        <div class="grid">
-          <a
-            href="https://www.behance.net/gallery/179360569/Koerber-Design-System-missing-protected-data"
-            target="_blank"
-          >
-            <figure class="effect-sadie">
-              <img
-                src="../../assets/portfolio/portfolio-img-4.jpg"
-                alt="Körber Design System"
-              />
-              <figcaption>
-                <h2>Körber Design System</h2>
-                <p>
-                  Körber Design System is a Design System for the all Körber digital products.   
-                  I designed and developed some of the Design
-                  System guidelines and components using
-                  <strong>>Web components</strong> with
-                  <strong>Lit</strong>,<strong>Typescript</strong> and
-                  <strong>Figma</strong>.
-                </p>
-              </figcaption>
-            </figure>
-          </a>
-        </div>
-      </div>
-    </div>
-    <div class="columns">
-      <div class="column">
-        <div class="grid">
-          <a href="https://www.susanasoarespinto.eu/" target="_blank">
-            <figure class="effect-sadie">
-              <img
-                src="../../assets/portfolio/portfolio-img-5.jpg"
-                alt="Susana Soares Pinto"
-              />
-              <figcaption>
-                <h2>Susana Soares Pinto</h2>
-                <p>
-                  Susana Soares Pinto is a portuguese multimedia and plastic
-                  artist. I designed and developed her website to showcase her
-                  work and exhibitions. The website includes a gallery of her
-                  work with a <strong>CMS</strong> to manage her own content
-                  autonomously.
-                </p>
-              </figcaption>
-            </figure>
-          </a>
-        </div>
-      </div>
-      <div class="column">
-        <div class="grid">
-          <a href="https://trustai.eu/" target="_blank">
-            <figure class="effect-sadie">
-              <img
-                src="../../assets/portfolio/portfolio-img-6.jpg"
-                alt="Trust AI"
-              />
-              <figcaption>
-                <h2>Trust AI</h2>
-                <p>
-                  Trust AI is a European Union project that I developing while
-                  working for LTPlabs. It presents a transparent, reliable and
-                  unbiased smart AI tool that's applied in specific use cases. I
-                  designed and developed the website with
-                  <strong>HTML5</strong>, <strong>SCSS</strong>,
-                  <strong>JavaScript</strong> and <strong>Jekyll</strong> while
-                  working at LTPlabs.
-                </p>
-              </figcaption>
-            </figure>
-          </a>
-        </div>
-      </div>
-    </div>
-    </div>
-  </section> -->
+  </section>
 </template>
 
 <style scoped lang="scss">
@@ -188,11 +189,5 @@
 
 img {
   width: 100%;
-}
-
-.column,
-.columns {
-  padding: 0;
-  margin: 0;
 }
 </style>
